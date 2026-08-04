3a880075189c85bdfe42e4e3688562aa624a3fdb
omishea <omarr.f@hotmail.com>
2026-08-05 00:10:16 +0200

    Update link text from 'Utforska produktsortimentet' to 'Upptäck Kollektionen'

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -87,7 +87,7 @@ function Index() {
                 href="#shop"
                 className="bg-primary text-primary-foreground hover:bg-primary/90 inline-flex h-11 items-center rounded-full px-7 text-sm font-medium transition-colors"
               >
-                Utforska produktsortimentet
+                Upptäck Kollektionen
               </a>
               <a
                 href="#about"
