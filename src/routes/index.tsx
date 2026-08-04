c90e443821a20eb6d40f0fa3c3ec57e47716e627
omishea <omarr.f@hotmail.com>
2026-08-04 23:41:08 +0200

    Adjust logo image scale in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -102,7 +102,7 @@ function Index() {
               alt="Shea Org logotyp"
               width={1254}
               height={1254}
-              className="h-full w-full scale-[1.35] object-contain"
+              className="h-full w-full scale-[1.25] object-contain"
             />
           </div>
         </section>
